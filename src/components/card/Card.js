552029c3f12dfc345ba51a265ac6772e7d9ecaaa
Ismail-Aslan <81377400+Ismail-Aslan@users.noreply.github.com>
2021-08-25 00:00:17 +0300

    x

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -2,15 +2,22 @@ import React,{useState} from 'react';
 
 import './Card.css'
 
-export default function Card() {
+export default function Card(props) {
     const [showLogo, setShowLogo] = useState(true);
   
     return(
         <div className="card" onClick={()=>setShowLogo(!showLogo)}>
             {showLogo ?  
-                <div className="div1"></div>
+                <>        
+                <img className="card-logo" src={props.cardInfo.img} alt="logo" />
+                <h3 className="card-title" >{props.cardInfo.name.toUpperCase()}</h3>
+                </>
                 : 
-                <div className="div2"></div>
+                <>
+                <p>{props.cardInfo.options[0]}</p>
+                <p>{props.cardInfo.options[1]}</p>
+                <p>{props.cardInfo.options[2]}</p>
+                </>
             }
         </div>
     )
